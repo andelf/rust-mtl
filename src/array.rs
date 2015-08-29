fd70359e5f78794c7f3e5caa71025ee0e6d1f93d
andelf <andelf@gmail.com>
2015-08-29 14:33:17 +0800

    add RefArray and Array::slice()

--- src/array.rs
+++ src/array.rs
@@ -1,9 +1,103 @@
 use std::fmt;
 use std::ops;
+use std::ops::{Range, RangeFrom, RangeTo, RangeFull};
 use std::iter;
 use std::iter::FromIterator;
 
+//
+pub trait ArrayIx {
+    fn to_idx_vec(&self, ax: usize, dims: &[usize]) -> Vec<usize>;
+    fn size(&self, ax: usize, dims: &[usize]) -> usize {
+        self.to_idx_vec(ax, dims).len()
+    }
+}
 
+impl ArrayIx for RangeFull {
+    fn to_idx_vec(&self, ax: usize, dims: &[usize]) -> Vec<usize> {
+        assert!(dims.len() > ax, "ax must be in range");
+        (0 .. dims[ax]).collect()
+    }
+}
+
+macro_rules! impl_array_ix_for_range {
+    ($typ:ty) => (
+        impl ArrayIx for Range<$typ> {
+            fn to_idx_vec(&self, ax: usize, dims: &[usize]) -> Vec<usize> {
+                assert!(dims.len() > ax, "ax must be in range");
+                assert!((self.start as usize) < dims[ax], "range start overflow");
+                assert!((self.end as usize) < dims[ax], "range start overflow");
+                (self.start as usize .. self.end as usize).map(|i| i as usize).collect()
+            }
+        }
+
+        impl ArrayIx for RangeFrom<$typ> {
+            fn to_idx_vec(&self, ax: usize, dims: &[usize]) -> Vec<usize> {
+                assert!(dims.len() > ax, "ax must be in range");
+                assert!((self.start as usize) < dims[ax], "range start overflow");
+                (self.start as usize .. dims[ax]).collect()
+            }
+        }
+
+        impl ArrayIx for RangeTo<$typ> {
+            fn to_idx_vec(&self, ax: usize, dims: &[usize]) -> Vec<usize> {
+                assert!(dims.len() > ax, "ax must be in range");
+                assert!((self.end as usize) < dims[ax], "range start overflow");
+                (0 .. self.end as usize).collect()
+            }
+        }
+    )
+}
+
+impl_array_ix_for_range!(i32);
+impl_array_ix_for_range!(usize);
+
+impl ArrayIx for [usize] {
+    fn to_idx_vec(&self, ax: usize, dims: &[usize]) -> Vec<usize> {
+        assert!(self.iter().filter(|&&i| i >= dims[ax]).count() == 0, "ax must be in range");
+        self.iter().map(|&i| i).collect()
+    }
+}
+
+impl ArrayIx for [i32] {
+    fn to_idx_vec(&self, _ax: usize, _dims: &[usize]) -> Vec<usize> {
+        self.iter().map(|&i| i as usize).collect()
+    }
+}
+
+
+macro_rules! impl_array_ix_for_fixed_size_array {
+    ($typ:ty, $size:expr) => (
+        impl ArrayIx for [$typ; $size] {
+            fn to_idx_vec(&self, _ax: usize, _dims: &[usize]) -> Vec<usize> {
+                self.iter().map(|&i| i as usize).collect()
+            }
+        }
+    )
+}
+
+impl_array_ix_for_fixed_size_array!(i32, 1);
+impl_array_ix_for_fixed_size_array!(i32, 2);
+impl_array_ix_for_fixed_size_array!(i32, 3);
+impl_array_ix_for_fixed_size_array!(i32, 4);
+impl_array_ix_for_fixed_size_array!(i32, 5);
+impl_array_ix_for_fixed_size_array!(i32, 6);
+impl_array_ix_for_fixed_size_array!(i32, 7);
+impl_array_ix_for_fixed_size_array!(i32, 8);
+
+#[test]
+fn test_index_ranges() {
+    let dims = vec![4, 5, 6];
+
+    assert_eq!((1..3).to_idx_vec(0, &dims), vec![1, 2]);
+    assert_eq!((1..).to_idx_vec(0, &dims), vec![1, 2, 3]);
+    assert_eq!((..2).to_idx_vec(0, &dims), vec![0, 1]);
+    assert_eq!((..).to_idx_vec(0, &dims), vec![0, 1, 2, 3]);
+}
+
+
+
+
+// nd Array
 #[derive(Clone, PartialEq, Debug)]
 pub struct Array<T> {
     data: Vec<T>,
@@ -24,6 +118,10 @@ impl<T: Copy> Array<T> {
             data: v,
             shape: vec![nelem]
         }
+    }
+
+    pub fn shape(&self) -> Vec<usize> {
+        self.shape.clone()
     }
 
     fn offset_of(&self, index: &[usize]) -> usize {
@@ -47,6 +145,13 @@ impl<T: Copy> Array<T> {
     pub fn reshape<D: AsRef<[usize]>>(&mut self, shape: D) {
         assert_eq!(self.data.len(), shape.as_ref().iter().product());
         self.shape = shape.as_ref().into()
+    }
+
+    pub fn slice<'a>(&'a self, ix: Vec<Box<ArrayIx>>) -> RefArray<'a, T> {
+        RefArray {
+            arr: self,
+            open_mesh: ix
+        }
     }
 }
 
@@ -74,6 +179,22 @@ impl<T: Copy, D: AsRef<[usize]>> ops::IndexMut<D> for Array<T> {
 }
 
 
+// impl<T: Copy> ops::Index<(Range<usize>,)> for Array<T> {
+//     type Output = T;
+
+//     #[inline]
+//     fn index<'a>(&'a self, index: (Range<usize,)) -> &'a T {
+
+//         println!("start => {}", index.0.start());
+//         println!("end => {}", index.0.start());
+//         &self.data[0]
+//     }
+// }
+
+
+
+
+// helper function for print an array
 fn dump_data<T: Copy + fmt::Display>(a: &Array<T>, dims: &[usize], nd: usize, index: &[usize]) -> String {
     let mut ret = String::new();
     if nd == 0 {
@@ -94,8 +215,8 @@ fn dump_data<T: Copy + fmt::Display>(a: &Array<T>, dims: &[usize], nd: usize, in
 
 impl<T: Copy + fmt::Display> fmt::Display for Array<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut ndim = self.shape.len();
-        let mut dims = self.shape.clone();
+        let ndim = self.shape.len();
+        let dims = self.shape.clone();
 
         let ret = dump_data(self, &dims, ndim, &vec![]);
         // FIXME: adding line break's buggy format
@@ -103,12 +224,76 @@ impl<T: Copy + fmt::Display> fmt::Display for Array<T> {
     }
 }
 
+// RefArray
+pub struct RefArray<'a, T: 'a> {
+    arr: &'a Array<T>,
+    // simulation of np.ix_ function?
+    open_mesh: Vec<Box<ArrayIx + 'a>>
+}
+
+impl<'a, T: Copy> RefArray<'a, T> {
+    pub fn shape(&self) -> Vec<usize> {
+        self.open_mesh.iter().enumerate().map(|(i,v)| (*v).size(i, &self.arr.shape())).collect()
+    }
+
+    fn offset_translate(&self, index: &[usize]) -> usize {
+        let mut ix = Vec::<usize>::new();
+        for (i, &k) in index.iter().enumerate() {
+            ix.push(self.open_mesh[i].to_idx_vec(i, &self.arr.shape())[k]);
+        }
+        self.arr.offset_of(&ix)
+    }
+
+    pub fn get<D: AsRef<[usize]>>(&self, index: D) -> T {
+        self.arr.data[self.offset_translate(index.as_ref())]
+    }
+
+    // pub fn get_mut<D: AsRef<[usize]>>(&mut self, index: D) -> &mut T {
+    //     let offset = self.offset_of(index.as_ref());
+    //     &mut self.data[offset]
+    // }
+}
+
+macro_rules! ix {
+    ($($arg:expr),*) => (
+        vec![$( Box::new($arg) as Box<ArrayIx> ),*]
+    )
+}
+
+impl<'a, T: Copy + fmt::Display> fmt::Display for RefArray<'a, T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fn dump_ref_array_data<'b, T: Copy + fmt::Display>(a: &'b RefArray<T>, dims: &[usize], nd: usize, index: &[usize]) -> String {
+            let mut ret = String::new();
+            if nd == 0 {
+                ret.push_str(&format!("{:4}", a.get(index)));
+            } else {
+                ret.push('[');
+                for i in 0 .. dims[0] {
+                    let index = index.iter().map(|&i| i).chain(iter::once(i)).collect::<Vec<usize>>();
+                    ret.push_str(&dump_ref_array_data(a, &dims[1..], nd-1, &index));
+                    if i < dims[0] - 1 {
+                        ret.push_str(", ");
+                    }
+                }
+                ret.push(']');
+            }
+            ret
+        }
+
+        let ndim = self.shape().len();
+        let dims = self.shape();
+
+        let ret = dump_ref_array_data(self, &dims, ndim, &vec![]);
+        // FIXME: adding line break's buggy format
+        write!(f, "{}", ret.replace("],", "],\n"))
+    }
+}
 
 #[test]
 fn test_array() {
     let mut v = Array::new(vec![ 0,  1,  2,  3,  4,  5,  6,  7,
-                             8,  9, 10, 11, 12, 13, 14, 15,
-                             16, 17, 18, 19, 20, 21, 22, 23]);
+                                 8,  9, 10, 11, 12, 13, 14, 15,
+                                 16, 17, 18, 19, 20, 21, 22, 23]);
 
     v.reshape([2, 3, 4]);
     assert_eq!(v.get([1, 1, 2]), 18);
@@ -117,7 +302,6 @@ fn test_array() {
     println!("fuck {:?}", v[[1,1,2]]);
 
     let shape = vec![2, 3, 4];
-    let mut offs = 0;
     let s = dump_data(&v, &shape, 3, &vec![]);
 
     println!("DEBUG print => \n{}", v);
@@ -125,4 +309,12 @@ fn test_array() {
 
     v.reshape([6, 4]);
     println!("DEBUG print => \n{}", v);
+
+    let v2 = ix!([1, 2, 3], [2, 0]);
+    let v3 = v.slice(v2);
+    println!("debug => {:?}", v3.get([0,0]));
+
+    println!("SUB[1,2,3; 2,0] => \n{}", v3);
+
+    println!("SUB => \n{}", v.slice(ix!(3.., [2,0])));
 }
